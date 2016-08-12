0b178f751b2f0d58979598708540e1e2d8e9800d
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-08-12 11:20:54 +0530

    Corrected java plugin compilation errors. Now default settings from Android Studio will be applied.

--- app/src/main/java/com/secretbiology/commonlibraries/MainActivity.java
+++ app/src/main/java/com/secretbiology/commonlibraries/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.secretbiology.helpers.general.Functions;
+import com.secretbiology.helpers.general.General;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Tag", Functions.timeStamp());
+        Log.i("Tag", General.timeStamp());
     }
 }
